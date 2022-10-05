6da285bc0d97508404f289cffdaf5ac21e8fd188
Dave Fisher <dave.j.fish@gmail.com>
2022-10-05 14:36:58 -0700

    auth sign-in/up working, catches and displays errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth/Auth';
 
 export default function App() {
-  return <></>;
+  return (
+    <Switch>
+      <Route path='/auth/:method' component={Auth} />
+    </Switch>
+  );
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -6,13 +6,13 @@ export function getUser() {
 
 export async function signUp(email, password) {
   const response = await client.auth.signUp({ email, password });
-  if (response.error) throw new Error(response.error);
+  if (response.error) throw new Error(response.error.message);
   return response.user;
 }
 
 export async function signIn(email, password) {
   const response = await client.auth.signIn({ email, password });
-  if (response.error) throw new Error(response.error);
+  if (response.error) throw new Error(response.error.message);
   return response.user;
 }
 
