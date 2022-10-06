971e3af8151d87c7c0884f88ef71cfe8246cbf58
Dave Fisher <dave.j.fish@gmail.com>
2022-10-06 16:55:48 -0700

    added bulma styling to auth, restructed getting the params

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,13 +12,13 @@ export default function Header() {
   };
 
   return (
-    <header className='navbar is-warning'>
+    <header className='navbar'>
       <span className='logo'>Recipes</span>
       {user
         ? <button className='button is-primary is-small' onClick={handleSignOut}>Sign Out</button>
         : <div className='header-right'>
-          <NavLink className='navbar-item' to='/auth/sign-in'>Sign In</NavLink>
-          <NavLink className='navbar-item' to='/auth/sign-up'>Sign Up</NavLink>
+          <NavLink className='button is-primary' to='/auth/sign-in'>Sign In</NavLink>
+          <NavLink className='button is-primary' to='/auth/sign-up'>Sign Up</NavLink>
         </div>}
     </header>
   );
