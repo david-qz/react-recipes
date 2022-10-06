8419224005b91e40a3b8a6f29c545b046ee9fc8d
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-06 15:18:39 -0700

    Only show edit button on user's own recipes

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,11 +1,14 @@
-export default function RecipeCard({ id, title, ingredients, instructions, handleEdit }) {
+import { useUser } from '../../context/UserContext';
+
+export default function RecipeCard({ id, user_id, title, ingredients, instructions, handleEdit }) {
+  const { user } = useUser();
 
   return (
     <div className='recipe-card'>
       <h4>{title}</h4>
       <p>{ingredients}</p>
       <p>{instructions}</p>
-      <button onClick={() => handleEdit(id)}>Edit</button>
+      {user.id === user_id && <button onClick={() => handleEdit(id)}>Edit</button>}
     </div>
   );
 }
