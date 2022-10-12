742f0a8a469d9f47423a511adcf8f92ef149f4ff
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-12 11:32:11 -0700

    Test that a logged in user can view recipes

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,9 @@ import { UserContextProvider } from './context/UserContext';
 import * as AuthService from './services/auth';
 jest.mock('./services/auth');
 
+import * as RecipesService from './services/recipes';
+jest.mock('./services/recipes');
+
 const mockUser = {
   id: '2fd26557-bf20-4d12-b187-22c6cec3dd54',
   aud: 'authenticated',
@@ -32,4 +35,35 @@ test('user can sign in', async () => {
 
   const signOutButton = await screen.findByText(/sign out/i);
   expect(signOutButton).toBeInTheDocument();
+});
+
+const mockRecipes = [
+  {
+    id: 0,
+    title: 'pork curry',
+    ingredients: 'pork, curry block, potatoes, carrots',
+    user_id: 0
+  },
+  {
+    id: 1,
+    title: 'beans and rice',
+    ingredients: 'beans, rice',
+    user_id: 0
+  },
+];
+
+test('user can see a list of recipes', async () => {
+  AuthService.getUser.mockReturnValue(mockUser);
+  RecipesService.getRecipes.mockReturnValue(mockRecipes);
+
+  render(
+    <BrowserRouter initialEntries={['/recipes']}>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </BrowserRouter>
+  );
+
+  await screen.findByText(/pork curry/i);
+  await screen.findByText(/beans and rice/i);
 });
