4cde4310b7e7c7f045ef65641fb0fdb77bcbd7b8
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-12 12:36:16 -0700

    Test new recipe functionality

--- src/App.test.js
+++ src/App.test.js
@@ -121,3 +121,36 @@ test('user can edit their own recipes', async () => {
   expect(editForm).not.toBeInTheDocument();
   await screen.findByText(/plain beans and rice/i);
 });
+
+test('user can add a recipe', async () => {
+  AuthService.getUser.mockReturnValue(mockUser);
+  RecipesService.getRecipes.mockReturnValue(mockRecipes);
+  RecipesService.addRecipe.mockReturnValue({
+    id: 2,
+    title: 'meat balls',
+    ingredients: 'meat',
+    user_id: '2fd26557-bf20-4d12-b187-22c6cec3dd54'
+  });
+
+  render(
+    <BrowserRouter initialEntries={['/recipes']}>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </BrowserRouter>
+  );
+
+  const addButton = await screen.findByText(/add recipe/i);
+  await act(async () => {
+    fireEvent.click(addButton);
+  });
+
+  const editForm = await screen.findByRole('form');
+  await act(async () => {
+    fireEvent.submit(editForm);
+  });
+
+  expect(RecipesService.addRecipe).toBeCalled();
+  expect(editForm).not.toBeInTheDocument();
+  await screen.findByText(/meat balls/i);
+});
