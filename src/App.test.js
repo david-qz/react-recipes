1335fe3df0072393b085b53df7108d1f69e95e16
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-12 11:18:24 -0700

    Test user sign in functionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { UserContextProvider } from './context/UserContext';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+import * as AuthService from './services/auth';
+jest.mock('./services/auth');
+
+const mockUser = {
+  id: '2fd26557-bf20-4d12-b187-22c6cec3dd54',
+  aud: 'authenticated',
+  role: 'authenticated',
+  email: 'user@test.com'
+};
+
+test('user can sign in', async () => {
+  AuthService.getUser.mockReturnValue(null);
+  AuthService.signIn.mockReturnValue(mockUser);
+
+  render(
+    <BrowserRouter>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </BrowserRouter>
+  );
+
+  const signInForm = screen.getByRole('form');
+  screen.getByLabelText(/email/i).value = mockUser.email;
+  screen.getByLabelText(/password/i).value = '123456';
+  fireEvent.submit(signInForm);
+
+  const signOutButton = await screen.findByText(/sign out/i);
+  expect(signOutButton).toBeInTheDocument();
 });
